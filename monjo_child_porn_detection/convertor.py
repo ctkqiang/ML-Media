fd59e61e57bdba1c14a1e3b85df954372168f6ef
John Melody Me <Johnmelodyme@yandex.com>
2020-06-10 12:10:54 +0800

    Update convertor.py

--- monjo_child_porn_detection/convertor.py
+++ monjo_child_porn_detection/convertor.py
@@ -4,6 +4,6 @@ from PIL import Image
 import numpy as np
 
 dataLocation = input("Please input the data:  \n")
-__image = Image.open(__dataLocation)
+__image = Image.open(dataLocation)
 np__image = np.array(__image)
 print(np__image)
