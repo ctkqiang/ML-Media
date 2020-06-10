fc1bc3542f6f2894dde24617437619d6d40b2412
John Melody Me <Johnmelodyme@yandex.com>
2020-06-10 12:05:17 +0800

    ..

--- monjo_child_porn_detection/convertor.py
+++ monjo_child_porn_detection/convertor.py
@@ -3,7 +3,7 @@ import PIL
 from PIL import Image
 import numpy as np
 
-__dataLocation = input()
-__image = Image.open(__dataLocation)
-np__image = np.array(__image)
-print(np__image)
+dataLocation = input("Please input the data:  \n")
+image = Image.open(dataLocation)
+np__image = np.array(image)
+print(np__image) 
